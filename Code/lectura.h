2940fc93123ede963d923222db75ff7392c0a8ce
flvr-soda <iearmada@proton.me>
2026-03-31 17:55:34 -0400

    Fix: arreglo de comentarios

--- Code/lectura.h
+++ Code/lectura.h
@@ -3,18 +3,18 @@
 #include <string.h>
 
 /*
- * Lee los datos de vehículos y el orden de ferrys desde el archivo de entrada.
- * Parámetros:
- *   lista: arreglo donde se almacenarán los vehículos leídos.
- *   n: puntero a entero donde se guardará la cantidad de vehículos leídos.
- *   orden: arreglo de tamaño 3 para guardar el orden de los ferrys.
- *   fname: nombre del archivo de entrada.
- * Retorna:
- *   0 si fue exitoso, un número negativo en caso de error.
+  Lee los datos de vehículos y el orden de ferrys desde el archivo de entrada.
+  Parámetros:
+    lista: arreglo donde se almacenarán los vehículos leídos.
+    n: puntero a entero donde se guardará la cantidad de vehículos leídos.
+    orden: arreglo de tamaño 3 para guardar el orden de los ferrys.
+    fname: nombre del archivo de entrada.
+  Retorna:
+    0 si fue exitoso, un número negativo en caso de error.
  */
 int leer_vehiculos(Vehiculo lista[], int *n, char orden[], const char* fname) {
     FILE *f = fopen(fname, "r");
-    if (!f) return -1;  // No se pudo abrir el archivo
+    if (!f) return -1;          // No se pudo abrir el archivo
     int i = 0;
     char line[128];
 
@@ -33,10 +33,10 @@ int leer_vehiculos(Vehiculo lista[], int *n, char orden[], const char* fname) {
         c = sscanf(line, "%d %d %d %d %d %d %d %s %d",
             &v.cod, &v.npa, &v.npt, &v.tpa, &v.tpt, &v.peso,
             &v.hora, v.placa, &v.tipo_ferry);
-        if (c < 9) break; // Si no se lograron leer todos los campos, termina
-        lista[i++] = v;   // Agrega el vehículo al arreglo
+        if (c < 9) break;           // Si no se lograron leer todos los campos, termina
+        lista[i++] = v;             // Agrega el vehículo al arreglo
     }
-    *n = i; // Cantidad de vehículos leídos
+    *n = i;                         // Cantidad de vehículos leídos
     fclose(f);
     return 0;
 }
